4defe17e32c9fefc985c4e51d4d06b83eeee4172
Tobias John <tobiajoh@ifi.uio.no>
2025-01-16 17:22:26 +0100

    start with EL reasoner oracle

---
+++ sut/reasoners/reasonerOracle.sh
@@ -0,0 +1,36 @@
+# tests, if the EL reasoners agree on the ontology
+# usage: ./suave_oracle.sh ONTOLOGY-FILE
+# result of oracle will be output on terminal (pass, fail)
+# pass: reasoners agree with each other
+# fail: reasoners do not agree with each other
+
+ontologyFile=$1
+
+ontologyFileContainer=testOnt2.owl
+
+CONTAINER_NAME=reasonerContainer
+
+
+running=1
+
+result=$( docker ps -q -f name=$CONTAINER_NAME )
+
+if ! [[ -n "$result" ]]; then
+  # container is not running
+  echo 'container is not running' 
+  docker start $CONTAINER_NAME
+  running=0
+fi
+
+sleep 1
+
+docker cp $ontologyFile $CONTAINER_NAME:/$ontologyFileContainer
+
+docker exec $CONTAINER_NAME ./reasonerOracle.sh ../$ontologyFileContainer
+
+
+# stop container if it was not running before this script was executed
+if [[ $running == 0 ]] ; then
+    echo 'stopping container'
+    docker stop $CONTAINER_NAME
+fi
