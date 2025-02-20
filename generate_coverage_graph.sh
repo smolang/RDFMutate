6cfd3ee091ea04bdf736f53e355fe703385c25f8
Tobias John <tobiajoh@ifi.uio.no>
2025-02-20 11:23:26 +0100

    adjust scripts

--- generate_coverage_graph.sh
+++ generate_coverage_graph.sh
@@ -2,7 +2,7 @@
 
 # optional argument: max heap size in GB
 
-heapLimit=${1:-16} # no limit: 16GB
+heapLimit=${1:-12} # no limit: 12GB
 limit=-Xmx${heapLimit}G 
 
 echo "evaluating input coverage. This can take several minutes. On our machine (Intel Core i7-1165G7) it took about 45 minutes."
@@ -18,3 +18,6 @@ java $limit -jar build/libs/OntoMutate-0.1.jar --el-graph --coverage-samples=500
 cd sut/reasoners/evaluation
 pdflatex inputCoverage.tex 
 cd ../../..
+
+mkdir -p results
+cp sut/reasoners/evaluation/inputCoverage.pdf results/inputCoverage.pdf
--- generate_coverage_graph_reduced.sh
+++ generate_coverage_graph_reduced.sh
@@ -2,7 +2,7 @@
 
 # optional argument: max heap size in GB
 
-heapLimit=${1:-8} # no limit: 16GB
+heapLimit=${1:-8} # no limit: 8GB
 limit=-Xmx${heapLimit}G 
 
 echo "evaluating input coverage using reduced sample size (50)"
@@ -18,3 +18,6 @@ java $limit -jar build/libs/OntoMutate-0.1.jar --el-graph --coverage-samples=50
 cd sut/reasoners/evaluation
 pdflatex inputCoverage.tex 
 cd ../../..
+
+mkdir -p results
+cp sut/reasoners/evaluation/inputCoverage.pdf results/inputCoverage.pdf
