94a3d027572e0ae9c957846d7f98a099c7560af2
Tobias John <tobiajoh@ifi.uio.no>
2025-06-25 12:01:09 +0200

    update scripts

--- generate_plots.sh
+++ generate_plots.sh
@@ -16,34 +16,38 @@ echo "build source code"
 # create coverage plots
 
 
-java $limit -jar build/libs/OntoMutate-0.1.jar --el-graph --coverage-samples=500
+java $limit -jar build/libs/RDFMutate-0.1.jar --el-graph --coverage-samples=500
 
-java $limit -jar build/libs/OntoMutate-0.1.jar --suave-coverage-graph --coverage-samples=100 # should be about 2h for 100 samples
+java $limit -jar build/libs/RDFMutate-0.1.jar --suave-coverage-graph --coverage-samples=100 # should be about 2h for 100 samples
 
 cd sut/reasoners/evaluation
 pdflatex inputCoverageEL.tex 
+pdflatex inputCoverageELBaseline.tex 
 cd ../../..
 
 cd sut/suave/evaluation
 pdflatex inputCoverageSuave.tex 
+pdflatex inputCoverageSuaveBaseline.tex 
 cd ../../..
 
-mkdir -p results
-cp sut/reasoners/evaluation/inputCoverageEl.pdf results/inputCoverageEl.pdf
-cp sut/suave/evaluation/inputCoverageSuave.pdf results/inputCoverageSuave.pdf
 
 echo "evaluating attempts per mask. This can take several minutes. On our machine (Intel Core i7-1165G7) it took about 1h."
 
 
 # create mask attempts plot
 echo "create attempts graph"
-java -jar build/libs/OntoMutate-0.1.jar --issre_graph --sample-size=100
+java -jar build/libs/RDFMutate-0.1.jar --issre_graph --sample-size=100
 
 
 cd sut/suave/evaluation
 pdflatex plotAttemps.tex 
 cd ../../..
 
+# copy results
 mkdir -p results
-cp sut/suave/evaluation/plotAttemps.pdf results/evaluation.pdf
+cp sut/suave/evaluation/plotAttemps.pdf results/maskEvaluation.pdf
+cp sut/reasoners/evaluation/inputCoverageEl.pdf results/inputCoverageEl.pdf
+cp sut/reasoners/evaluation/inputCoverageElBaseline.pdf results/inputCoverageElBaseline.pdf
+cp sut/suave/evaluation/inputCoverageSuave.pdf results/inputCoverageSuave.pdf
+cp sut/suave/evaluation/inputCoverageSuaveBaseline.pdf results/inputCoverageSuaveBaseline.pdf
 
--- generate_plots_reduced.sh
+++ generate_plots_reduced.sh
@@ -19,16 +19,18 @@ echo "evaluating input coverage using reduced sample size (20 and 5)"
 
 
 
-java $limit -jar build/libs/OntoMutate-0.1.jar --el-graph --coverage-samples=20
+java $limit -jar build/libs/RDFMutate-0.1.jar --el-graph --coverage-samples=20
 
-java $limit -jar build/libs/OntoMutate-0.1.jar --suave-coverage-graph --coverage-samples=5 
+java $limit -jar build/libs/RDFMutate-0.1.jar --suave-coverage-graph --coverage-samples=5 
 
 cd sut/reasoners/evaluation
 pdflatex inputCoverageEL.tex 
+pdflatex inputCoverageELBaseline.tex 
 cd ../../..
 
 cd sut/suave/evaluation
 pdflatex inputCoverageSuave.tex 
+pdflatex inputCoverageSuaveBaseline.tex 
 cd ../../..
 
 
@@ -37,7 +39,7 @@ echo "evaluating attempts per mask. This can take several minutes."
 
 # create mask attempts plot
 echo "create attempts graph"
-java -jar build/libs/OntoMutate-0.1.jar --issre_graph --sample-size=5
+java -jar build/libs/RDFMutate-0.1.jar --issre_graph --sample-size=5
 
 
 cd sut/suave/evaluation
@@ -48,7 +50,8 @@ cd ../../..
 mkdir -p results
 cp sut/suave/evaluation/plotAttemps.pdf results/maskEvaluation.pdf
 cp sut/reasoners/evaluation/inputCoverageEl.pdf results/inputCoverageEl.pdf
+cp sut/reasoners/evaluation/inputCoverageElBaseline.pdf results/inputCoverageElBaseline.pdf
 cp sut/suave/evaluation/inputCoverageSuave.pdf results/inputCoverageSuave.pdf
-
+cp sut/suave/evaluation/inputCoverageSuaveBaseline.pdf results/inputCoverageSuaveBaseline.pdf
 
 
